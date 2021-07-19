e7e284ea773753c883dff0584d5a4a1461b91596
Andrew <andrewembury@me.com>
2021-07-19 15:06:44 -0800

    intial testing work

--- tests/header.test.js
+++ tests/header.test.js
@@ -4,7 +4,7 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../components/header/Header';
 
-describe('App', () => {
+describe('Header', () => {
   it('renders without crashing', () => {
     render(<Header />);
     expect(
